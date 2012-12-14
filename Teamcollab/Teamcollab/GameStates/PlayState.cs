2d4a184298beb33dc6b227224ef4e74d29082eca
Martin Grönlund <martingronlund@live.se>
2012-12-15 00:40:03 +0100

    Added PlayState boilerplate

--- Teamcollab/Teamcollab/GameStates/PlayState.cs
+++ Teamcollab/Teamcollab/GameStates/PlayState.cs
@@ -1,11 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Microsoft.Xna.Framework;
 
 namespace Teamcollab.GameStates
 {
+  /// <summary>
+  /// Handles the in-game logic
+  /// </summary>
   class PlayState : GameState
   {
+    #region Properties
+    #endregion
+
+    public PlayState()
+      :base()
+    {
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+    }
+
+    public override void Draw()
+    {
+    }
   }
 }
