3cf5d5cfa536d1cb445d3a1e997b29b91ef54d96
Martin Grönlund <martingronlund@live.se>
2012-12-14 19:51:00 +0100

    Test commit

---
+++ Teamcollab/Teamcollab/TestCommit.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Teamcollab
+{
+    /// <summary>
+    /// Tests GitHub commit in a pro manner
+    /// </summary>
+    class TestCommit
+    {
+        // Test test
+    }
+}
+
