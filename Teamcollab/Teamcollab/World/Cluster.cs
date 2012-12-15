e8995c55bc4f9d7d7eb4eb068a364d110f554ec6
Martin Grönlund <martingronlund@live.se>
2012-12-15 19:40:06 +0100

    Initial tile and cluster creation

---
+++ Teamcollab/Teamcollab/World/Cluster.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Teamcollab.World
+{
+  /// <summary>
+  /// Handles a collection of Tiles
+  /// </summary>
+  class Cluster
+  {
+    Tile[,] tiles;
+
+    public Cluster()
+    {
+      tiles = new Tile[128, 128];
+
+      for (int y = 0; y < tiles.GetLength(1); y++)
+      {
+        for (int x = 0; x < tiles.GetLength(0); x++)
+        {
+          tiles[x, y] = new Tile(TileType.Grass);
+        }
+      }
+    }
+  }
+}
