64553dc64405efe4e96afc3aca8673a336751ae8
Martin Grönlund <martingronlund@live.se>
2012-12-15 19:40:06 +0100

    Initial tile and cluster creation

---
+++ Teamcollab/Teamcollab/World/Tile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Teamcollab.World
+{
+  /// <summary>
+  /// Holds all tile types
+  /// </summary>
+  public enum TileType : byte
+  {
+    Undefined = 0,
+
+    Grass,
+    Water,
+  }
+
+  /// <summary>
+  /// One tiny square of the world
+  /// </summary>
+  public struct Tile
+  {
+    TileType type;
+
+    public Tile(TileType type)
+    {
+      this.type = type;
+    }
+  }
+}
