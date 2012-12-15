0d164a310260d3094f98a7e4b89d1589cf726481
Peter Mårtensson <veritas.is.me@gmail.com>
2012-12-15 19:18:24 +0100

    Added basic ApplicationSettings struct.
    Signed-off-by: Peter Mårtensson <veritas.is.me@gmail.com>

---
+++ Teamcollab/Teamcollab/DataSerialization/ApplicationSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Teamcollab.DataSerialization
+{
+  [Serializable]
+  public struct ApplicationSettings
+  {
+    public int ScreenHeight { get; private set; }
+    public int ScreenWidth { get; private set; }
+
+
+    /// <summary>
+    /// Obtain a default initialized ApplicationSettings object.
+    /// </summary>
+    /// <returns></returns>
+    public static ApplicationSettings GetDefault()
+    {
+      ApplicationSettings appSettings;
+      appSettings.ScreenWidth = 1280;
+      appSettings.ScreenHeight = 720;
+
+      return appSettings;
+    }
+  }
+}
