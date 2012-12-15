396e220e79b8ce729e7846b70eb6a1ddac9c88e0
Martin Grönlund <martingronlund@live.se>
2012-12-15 20:15:55 +0100

    Added preliminary functionality to tiles and clusters

--- Teamcollab/Teamcollab/World/Cluster.cs
+++ Teamcollab/Teamcollab/World/Cluster.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
 namespace Teamcollab.World
 {
@@ -20,6 +19,37 @@ namespace Teamcollab.World
       {
         for (int x = 0; x < tiles.GetLength(0); x++)
         {
+          tiles[x, y] = new Tile(TileType.Grass);
+        }
+      }
+    }
+
+    /// <summary>
+    /// Draws the cluster
+    /// </summary>
+    /// <param name="spriteBatch">Spritebatch used for drawing</param>
+    public void Draw(SpriteBatch spriteBatch, GUI.Camera2D camera)
+    {
+      for (int y = 0; y < tiles.GetLength(1); y++)
+      {
+        for (int x = 0; x < tiles.GetLength(0); x++)
+        {
+          Tile tile = tiles[x, y];
+          switch (tile.type)
+          {
+            case TileType.Undefined:
+              throw new NotImplementedException();
+            case TileType.Grass:
+            case TileType.Water:
+              Rectangle bounds = Tile.TileBounds(x, y);
+              if (camera.Bounds.Intersects(bounds))
+              {
+                spriteBatch.Draw(tile.GetTexture(), bounds, Color.White);
+              }
+              break;
+            default:
+              throw new NotImplementedException();
+          }
           tiles[x, y] = new Tile(TileType.Grass);
         }
       }
--- Teamcollab/Teamcollab/World/Tile.cs
+++ Teamcollab/Teamcollab/World/Tile.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Teamcollab.World
 {
@@ -21,11 +23,42 @@ namespace Teamcollab.World
   /// </summary>
   public struct Tile
   {
-    TileType type;
+    #region Constants
+    static readonly Point size = new Point(32, 32);
+    #endregion
+
+    #region Members
+    public TileType type;
+    #endregion
 
     public Tile(TileType type)
     {
       this.type = type;
+    }
+
+    /// <summary>
+    /// Returns the cluster-based bounds of this tile,
+    /// as opposed to world bounds
+    /// </summary>
+    /// <param name="x">X index</param>
+    /// <param name="y">Y index</param>
+    /// <returns>Tell me if you see this anywhere</returns>
+    public static Rectangle TileBounds(int x, int y)
+    {
+      Rectangle result = new Rectangle(
+        x * size.X,
+        y * size.Y,
+        size.X,
+        size.Y
+      );
+      return result;
+    }
+
+    public Texture2D GetTexture()
+    {
+      if (type == TileType.Undefined)
+        return null;
+      return Library.textures[type.ToString()];
     }
   }
 }
