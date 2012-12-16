0f79919fb41a9481cb7eb081bad601db2a4f191e
Peter Mårtensson <veritas.is.me@gmail.com>
2012-12-16 01:02:20 +0100

    Added Worldmanager
    Signed-off-by: Peter Mårtensson <veritas.is.me@gmail.com>

---
+++ Teamcollab/Teamcollab/Engine/World/WorldManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Teamcollab.Engine.World
+{
+  sealed class WorldManager
+  {
+    #region Properties
+
+    #endregion
+
+    #region Members
+
+    #endregion
+
+    public WorldManager()
+    {
+
+    }
+
+    public void Update(GameTime gameTime)
+    {
+
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+
+    }
+  }
+}
