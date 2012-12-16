7dff325ef31bb7cd24260e3c767ef4b4ef0ace1f
Martin Grönlund <martingronlund@live.se>
2012-12-16 04:56:59 +0100

    Added Entity.cs and DrawableEntity.cs.

---
+++ Teamcollab/Teamcollab/GameObjects/Entity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Teamcollab.Resources;
+using Microsoft.Xna.Framework;
+
+namespace Teamcollab.GameObjects
+{
+  /// <summary>
+  /// Holds all entity types
+  /// </summary>
+  enum EntityType
+  {
+    Undefined = 0,
+
+    Player,
+  }
+
+  /// <summary>
+  /// A blueprint for entities
+  /// </summary>
+  abstract class Entity
+  {
+    #region Properties
+    #endregion
+
+    #region Members
+    protected EntityType entityType;
+    #endregion
+
+    public Entity()
+    {
+    }
+
+    /// <summary>
+    /// Used by the entity to update itself
+    /// </summary>
+    /// <param name="gameTime"></param>
+    public abstract void Update(GameTime gameTime);
+  }
+}
