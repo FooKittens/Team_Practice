5a30c67a312ca4daafcc9800df3f06696f16803f
Peter Mårtensson <veritas.is.me@gmail.com>
2012-12-16 02:19:33 +0100

    Working with basic tile management
    Signed-off-by: Peter Mårtensson <veritas.is.me@gmail.com>

--- Teamcollab/Teamcollab/Engine/Helpers/Coordinates.cs
+++ Teamcollab/Teamcollab/Engine/Helpers/Coordinates.cs
@@ -1,10 +1,16 @@
-﻿namespace Teamcollab.Engine.Helpers
+﻿using Microsoft.Xna.Framework;
+namespace Teamcollab.Engine.Helpers
 {
   /// <summary>
   /// Two integers x,y representing coordinates
   /// </summary>
   public struct Coordinates
   {
+
+    #region Static Caching
+    private static readonly Coordinates zero = new Coordinates(0, 0); 
+    #endregion
+
     public int X;
     public int Y;
 
@@ -13,6 +19,8 @@
       X = x;
       Y = y;
     }
+
+    public static Coordinates Zero { get { return zero; } }
 
     public static Coordinates operator +(Coordinates lhs, Coordinates rhs)
     {
@@ -40,6 +48,16 @@
       lhs.X /= rhs.X;
       lhs.Y /= rhs.Y;
       return lhs;
+    }
+
+    public static implicit operator Vector2(Coordinates c)
+    {
+      return new Vector2(c.X, c.Y);
+    }
+
+    public static implicit operator Point(Coordinates c)
+    {
+      return new Point(c.X, c.Y);
     }
   }
 }
