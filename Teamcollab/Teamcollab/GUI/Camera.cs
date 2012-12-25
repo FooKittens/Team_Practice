da09e7736933c614515ac02c82742064f11e9a1a
Martin Grönlund <martingronlund@live.se>
2012-12-25 15:55:04 +0100

    Test changes, mostly stashed

--- Teamcollab/Teamcollab/GUI/Camera.cs
+++ Teamcollab/Teamcollab/GUI/Camera.cs
@@ -164,7 +164,12 @@ namespace Teamcollab.GUI
     /// <returns>World coordinate</returns>
     static public Vector2 TranslatePositionByCamera(Vector2 camCoord)
     {
+      camCoord = Vector2.Transform(camCoord, Isometric);
       return Vector2.Transform(camCoord, translationMatrix);
     }
+
+    static Matrix Isometric =
+    Matrix.CreateRotationZ(MathHelper.PiOver4) *
+    Matrix.CreateScale((float)Math.Sqrt(2f) / 2.0f, (float)Math.Sqrt(2f) / 4.0f, 1);
   }
 }
